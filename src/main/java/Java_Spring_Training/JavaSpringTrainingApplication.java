0eb8f5f81f3c628d1ca692ef8c919be9f2ca0b9d
Mjeevanantham <jeevanantham.cb20@bitsathy.ac.in>
2024-10-21 23:26:42 +0530

    Initial commit

---
+++ src/main/java/Java_Spring_Training/JavaSpringTrainingApplication.java
@@ -0,0 +1,13 @@
+package Java_Spring_Training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Java_Spring_Training/JavaSpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package Java_Spring_Training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
